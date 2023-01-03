fd20f785b4c97980fa7e6f9266b351d0863ee0ae
madan <reddymadan0512#gmail.com>
2023-01-04 00:52:56 +0530

    aaa

--- AM project/Electronic shoppig/index.js
+++ AM project/Electronic shoppig/index.js
@@ -1,30 +1,33 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// image sliders
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// const buttons = document.querySelectorAll("[data-carousel-button]");
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+//     const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+//     const activeslide = slides.querySelector("[data-active]");
+//     let newIndex = [...slides.children].indexOf(activeslide) + offset;
+//     if (newIndex < 0) newIndex = slides.children.length - 1;
+//     if (newIndex >= slides.children.length) newIndex = 0;
+//     slides.children[newIndex].dataset.active = true;
+//     delete activeslide.dataset.active;
+//   });
+// });
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
