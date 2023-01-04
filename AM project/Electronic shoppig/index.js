311ca2ebd1e5f076ee09786e1caf681a8031b6e7
madan <reddymadan0512#gmail.com>
2023-01-04 08:47:17 +0530

    mm

--- AM project/Electronic shoppig/index.js
+++ AM project/Electronic shoppig/index.js
@@ -1,33 +1,33 @@
-// image sliders
-
-// const buttons = document.querySelectorAll("[data-carousel-button]");
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-//     const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
-
-//     const activeslide = slides.querySelector("[data-active]");
-//     let newIndex = [...slides.children].indexOf(activeslide) + offset;
-//     if (newIndex < 0) newIndex = slides.children.length - 1;
-//     if (newIndex >= slides.children.length) newIndex = 0;
-//     slides.children[newIndex].dataset.active = true;
-//     delete activeslide.dataset.active;
-//   });
-// });
-
-const buttons = document.querySelectorAll("[data-carousel-button]");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
-
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+// image
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  const items = carousel.querySelectorAll(".carousel-item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel-button"></span>`;
   });
+
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `
+		<div class="carousel-nav">
+			${buttonsHtml.join("")}
+		</div>
+	`
+  );
+
+  const buttons = carousel.querySelectorAll(".carousel-button");
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      // un-select all the items
+      items.forEach((item) => item.classList.remove("carousel-selected"));
+      buttons.forEach((button) => button.classList.remove("carousel-button-selected"));
+
+      items[i].classList.add("carousel-selected");
+      button.classList.add("carousel-button-selected");
+    });
+  });
+
+  // Select the first item on page load
+  items[0].classList.add("carousel-selected");
+  buttons[0].classList.add("carousel-button-selected");
 });
